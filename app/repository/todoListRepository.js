ba80b970fd741f6299972c6fb96bcfb19175e0ef
lingaimu <774947332@qq.com>
2018-05-16 17:57:56 +0800

    wangyuxiao first commit

---
+++ app/repository/todoListRepository.js
@@ -0,0 +1,60 @@
+const Todo = require("../models/todo");
+
+let currentId = 0;
+
+class TodoListRepository {
+  constructor() {
+    const todo1 = new Todo(++currentId, "todo1", "todo1 desc");
+    const todo2 = new Todo(++currentId, "todo2", "todo2 desc");
+    todo1.isFinished = true;
+    this.todoList = [todo1, todo2];
+  }
+
+  listAllTodos() {
+    //实现查看所有todos的方法
+    // console.log(this.todoList.todo1);
+    // console.log(tog("nihis.todoList.todo2);
+    for (const obj in this.todoList) {
+        console.log(obj);
+    }
+    return this.todoList;
+  }
+
+  findTodoBy(id) {
+    //实现通过id查看具体todo的方法
+    for (const obj in this.todoList) {
+      // console.log(obj);
+      // console.log("sjfisj")
+        if(obj == id){
+            // console.log(this.todoList[obj]);
+            return this.todoList[obj-1];
+        }
+    }
+  }
+
+  createTodo(todoBody) {
+    //实现创建新todo纪录的方法
+    ++currentId
+    var str = "todo"+currentId;
+    const str = new Todo(currentId, "todo1", "todo1 desc");
+    this.todoList.push(str);
+    this.listAllTodos();
+  }
+
+  updateTodo(id, update) {
+    //实现通过id和一个更新对象来更新todo纪录的方法
+    for (const obj in this.todoList) {
+        if(obj.currentId == id)
+            obj = update;
+    }
+    console.log(obj)
+  }
+
+  deleteTodoBy(id) {
+    //实现通过id来删除todo纪录的方法
+    // delete this.todoList[id];
+    this.todoList.reduce[id];
+  }
+}
+
+module.exports = new TodoListRepository();
